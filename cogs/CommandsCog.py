5755c2b64bc3201c39e32170be093ce2722802db
Zyroad <52015010+ZyroadGM@users.noreply.github.com>
2020-08-22 14:13:44 +0300

    better help command

--- cogs/CommandsCog.py
+++ cogs/CommandsCog.py
@@ -15,15 +15,17 @@ class Commands(commands.Cog):
             colour=discord.Color.dark_blue(),
         )
         embed.set_footer(text=f"")
-        embed.add_field(name='----Common Commands----', value="*** ***")
-        embed.add_field(name='----Administrator Commands----', value="*** ***")
-        embed.add_field(name='wtismysrpx', value="Shows your server's prefix!")
-        embed.add_field(name='set_prefix <prefix>{"setprefix", "set-prefix"}', value="You can set your custom prefix for the bot")
-        embed.add_field(name='help {"h", "cmd", "commands"}', value="Sends a list of all commands")
-        embed.add_field(name='set_trigger_value <trigger_value>{"stv", "set-trigger-value"}', value='You can set your custom trigger value for the "AutomaticRoles"')
-        embed.add_field(name='trigger_value {trigger-value}', value="Shows your server's trigger value for the \"AutomaticRoles\"")
-        embed.add_field(name='on', value="Turns on automatic roles")
-        embed.add_field(name='off', value="Turns off automatic roles")
+        embed.add_field(name='***Information Commands:***', value="*** ***", inline=False)
+        embed.add_field(name='wtismysrpx', value="Shows your server's prefix!", inline=False)
+        embed.add_field(name='help {"h", "cmd", "commands"}', value="Sends a list of all commands", inline=False)
+        embed.add_field(name='trigger_value {trigger-value}',
+                        value="Shows your server's trigger value for the \"AutomaticRoles\"", inline=False)
+        embed.add_field(name='*Administrator Commands:*', value="*** ***", inline=False)
+        embed.add_field(name='set_prefix <prefix>{"setprefix", "set-prefix"}',
+                        value="You can set your custom prefix for the bot", inline=False)
+        embed.add_field(name='set_trigger_value <trigger_value>{"stv", "set-trigger-value"}', value='You can set your custom trigger value for the "AutomaticRoles"', inline=False)
+        embed.add_field(name='on', value="Turns on automatic roles", inline=False)
+        embed.add_field(name='off', value="Turns off automatic roles", inline=False)
         await ctx.send(embed=embed)
 
 
