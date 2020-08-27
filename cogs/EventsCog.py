e1501fa99f7bc3e3cb5ac643c747c3baf75072b5
Zyroad <52015010+ZyroadGM@users.noreply.github.com>
2020-08-27 17:19:01 +0300

    Update EventsCog.py

--- cogs/EventsCog.py
+++ cogs/EventsCog.py
@@ -2,10 +2,13 @@
 import discord
 from discord.ext import commands
 # ----------------------------
-# Json Package Import
-import json
+# Database Package Import
+import pymongo
+from pymongo import MongoClient
 # ----------------------------
+import os
 
+db = MongoClient("database")
 
 class EventsCog(commands.Cog):
     def __init__(self, client):
@@ -13,46 +16,26 @@ class EventsCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        guild_data = json.load(open("data/guild_data.json"))
-        guild_data['guilds'].append(
-            {
-                guild.id:
-                    {
-                        "prefix": "+",
-                        "AutomaticRoles": "OFF",
-                        "trigger_value": "2"
-                    }
-            }
-        )
-        with open('data/guild_data.json', 'w') as outfile:
-            json.dump(guild_data, outfile, indent=2)
-        outfile.close()
-        await guild.create_role(name="-~~~Automatic Roles~~~-", colour=discord.Colour("#ffffff"))
+        try:
+            db.insert_one({"_id": str(guild.id), "prefix": "+", "AutomaticRoles": "OFF", "trigger_value": "2"})
+        except:
+            print("Already in guild")
+        await guild.create_role(name="-~~~Automatic Roles~~~-", colour=discord.Colour.lighter_grey())
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        await ctx.send(error)
+        print(error)
+
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.content == "wtismysrpx":
-            guild_data = json.load(open("data/guild_data.json"))
-            guild = message.guild
-            prefix = None
-            for guild_data_in_list in guild_data['guilds']:
-                try:
-                    # Get the prefix from the guild dict
-                    prefix = guild_data_in_list[str(guild.id)]['prefix']
-                except:
-                    continue
+            prefix = db.find_one({"_id": str(message.guild.id)})["prefix"]
             await message.channel.send(f"Your server's prefix is \"`{prefix}`\"")
 
     @commands.Cog.listener()
     async def on_guild_unavailable(self, guild):
-        guild_data = json.load(open("data/guild_data.json"))
-        for guild_data_in_list in guild_data['guilds']:
-            del (guild_data_in_list[str(guild.id)])
-        json.dump(guild_data, open("data/guild_data.json", "w"), indent=2)
+        db.delete_one({"_id": str(guild.id)})
 
 
 def setup(client):
