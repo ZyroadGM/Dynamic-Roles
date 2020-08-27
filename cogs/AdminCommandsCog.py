2856e7465bbb6383c8e1d91dd1cda0c9304404e5
Zyroad <52015010+ZyroadGM@users.noreply.github.com>
2020-08-27 17:17:31 +0300

    Update AdminCommandsCog.py

--- cogs/AdminCommandsCog.py
+++ cogs/AdminCommandsCog.py
@@ -3,7 +3,12 @@ from discord.ext import commands
 # ----------------------------
 # Json Package Import
 import json
+import os
 # ----------------------------
+import pymongo
+from pymongo import MongoClient
+# ----------------------------
+db = MongoClient("database")
 
 
 class AdminCommands(commands.Cog):
@@ -14,56 +19,31 @@ class AdminCommands(commands.Cog):
     @commands.command(aliases=["stv", "set-trigger-value"], case_insensitive=True)
     @commands.guild_only()
     async def set_trigger_value(self, ctx, *, trigger_value):
-        guild_data = json.load(open("data/guild_data.json"))
-        for guild_data_in_list in guild_data['guilds']:
-            try:
-                guild_data_in_list[str(ctx.guild.id)]['trigger_value'] = trigger_value
-                pass
-            except:
-                continue
-        json.dump(guild_data, open("data/guild_data.json", "w"), indent=2)
+        if isinstance(int(trigger_value), int) is False:
+            await ctx.send("trigger_value is not an integer!")
+            return
+        db.update_one({"_id": str(ctx.guild.id)}, {"$set": {"trigger_value": trigger_value}})
         await ctx.send(f"Trigger value for \"AutomaticRoles\" is set to `{trigger_value}`!")
 
     @commands.has_permissions(administrator=True)
     @commands.command(aliases=["set-prefix", "setprefix"], case_insensitive=True)
     @commands.guild_only()
     async def set_prefix(self, ctx, *, prefix):
-        guild_data = json.load(open("data/guild_data.json"))
-        for guild_data_in_list in guild_data['guilds']:
-            try:
-                guild_data_in_list[str(ctx.guild.id)]['prefix'] = prefix
-                pass
-            except:
-                continue
-        json.dump(guild_data, open("data/guild_data.json", "w"), indent=2)
-        await ctx.send(f"Prefix is set to `{prefix}`!")
+        db.update_one({"_id": str(ctx.guild.id)}, {"$set": {"prefix": prefix}})
+        await ctx.send(f"Prefix is set to \"`{prefix}`\"!")
 
     @commands.has_permissions(administrator=True)
     @commands.command(case_insensitive=True)
     @commands.guild_only()
     async def on(self, ctx):
-        guild_data = json.load(open("data/guild_data.json"))
-        for guild_data_in_list in guild_data['guilds']:
-            try:
-                guild_data_in_list[str(ctx.guild.id)]['AutomaticRoles'] = "ON"
-                pass
-            except:
-                continue
-        json.dump(guild_data, open("data/guild_data.json", "w"), indent=2)
+        db.update_one({"_id": str(ctx.guild.id)}, {"$set": {"AutomaticRoles": "ON"}})
         await ctx.send(f"AutomaticRoles is \"ON\"!")
 
     @commands.has_permissions(administrator=True)
     @commands.command(case_insensitive=True)
     @commands.guild_only()
     async def off(self, ctx):
-        guild_data = json.load(open("data/guild_data.json"))
-        for guild_data_in_list in guild_data['guilds']:
-            try:
-                guild_data_in_list[str(ctx.guild.id)]['AutomaticRoles'] = "OFF"
-                pass
-            except:
-                continue
-        json.dump(guild_data, open("data/guild_data.json", "w"), indent=2)
+        db.update_one({"_id": str(ctx.guild.id)}, {"$set": {"AutomaticRoles": "OFF"}})
         await ctx.send(f"AutomaticRoles is \"OFF\"!")
 
 
