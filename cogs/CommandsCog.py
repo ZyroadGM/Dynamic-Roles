741d6f785abf80bd45e34769df47377a6b4c3ff6
Zyroad <52015010+ZyroadGM@users.noreply.github.com>
2020-08-29 11:05:59 +0300

    Update CommandsCog.py

--- cogs/CommandsCog.py
+++ cogs/CommandsCog.py
@@ -2,6 +2,13 @@
 import discord
 from discord.ext import commands
 # ----------------------------
+# Database Package Import
+import pymongo
+from pymongo import MongoClient
+# ----------------------------
+import os
+
+db = MongoClient("database")
 
 
 class Commands(commands.Cog):
@@ -10,22 +17,23 @@ class Commands(commands.Cog):
 
     @commands.command(aliases=['h', 'cmd', 'commands'], case_insensitive=True)
     async def help(self, ctx):
+        prefix = db.find_one({"_id": str(ctx.guild.id)})["prefix"]
         embed = discord.Embed(
             colour=discord.Color.dark_blue(),
         )
         embed.set_footer(text=f"")
         embed.add_field(name='***ℹ️ Information Commands:***', value="*** ***", inline=False)
-        embed.add_field(name='`wtismysrpx`', value="Shows your server's prefix! (No prefix before this command)", inline=False)
-        embed.add_field(name='`help` {"h", "cmd", "commands"}', value="Sends a list of all commands", inline=False)
-        embed.add_field(name='`trigger_value` {trigger-value}',
+        embed.add_field(name='`wtismysrpx`', value="Shows your server's prefix!", inline=False)
+        embed.add_field(name=prefix + '`help` {"h", "cmd", "commands"}', value="Sends a list of all commands", inline=False)
+        embed.add_field(name=prefix + '`trigger_value` {trigger-value}',
                         value="Shows your server's trigger value for the \"AutomaticRoles\"", inline=False)
         embed.add_field(name='***🖥️ Administrator Commands:***', value="*** ***", inline=False)
-        embed.add_field(name='`set_prefix` <prefix> {"setprefix", "set-prefix"}',
+        embed.add_field(name=prefix + '`set_prefix` <prefix> {"setprefix", "set-prefix"}',
                         value="You can set your custom prefix for the bot", inline=False)
-        embed.add_field(name='`set_trigger_value` <trigger_value> {"stv", "set-trigger-value"}',
+        embed.add_field(name=prefix + '`set_trigger_value` <trigger_value> {"stv", "set-trigger-value"}',
                         value='You can set your custom trigger value for the "AutomaticRoles"', inline=False)
-        embed.add_field(name='`on`', value="Turns on automatic roles", inline=False)
-        embed.add_field(name='`off`', value="Turns off automatic roles", inline=False)
+        embed.add_field(name=prefix + '`on`', value="Turns on automatic roles", inline=False)
+        embed.add_field(name=prefix + '`off`', value="Turns off automatic roles", inline=False)
         await ctx.send(embed=embed)
 
 
